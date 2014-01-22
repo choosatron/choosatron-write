24c275040a494683b89f3960256c072b6e87efe7
Jerry <jerry@monkeytheater.com>
2014-01-22 17:20:45 -0600

    Initial app structure checkin.

---
+++ js/background.js
@@ -0,0 +1,13 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('window.html', {
+    'bounds': {
+      'width': 400,
+      'height': 500
+    }
+  });
+});
