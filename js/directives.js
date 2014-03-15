096b6c429f87e0e933648d2b88b0b5553d5453dd
Zachary Johnson <me@zachstronaut.com>
2014-03-15 17:00:29 -0500

    include ending color value with passage icon

--- js/directives.js
+++ js/directives.js
@@ -1,20 +1,20 @@
 
 app.directive('confirmClick', ['$parse', function ($parse) {
 	return {
-		compile: function($element, attr) {
-			var fn = $parse(attr.confirmClick);
+		compile: function($templateElement, $templateAttributes) {
+			var fn = $parse($templateAttributes.confirmClick);
 
-			return function (scope, element, attr) {
-				element.on('click', function (event) {
+			return function ($scope, $element, attrs) {
+				$element.on('click', function (event) {
 					$('#confirmDelete').off('click.confirmed');
 					$('#confirmDelete').one('click.confirmed', function (evt2) {
-						scope.$apply(function () {
-							fn(scope, {$event: evt2});
+						$scope.$apply(function () {
+							fn($scope, {$event: evt2});
 						});
 					});
 
-					scope.$apply(function () {
-						scope.modal.confirm_message = attr.confirmMessage;
+					$scope.$apply(function () {
+						$scope.modal.confirm_message = attrs.confirmMessage;
 					});
 					
 					$('#confirmModal').modal('show');
@@ -24,4 +24,22 @@ app.directive('confirmClick', ['$parse', function ($parse) {
 	};
 }]);
 
+app.directive('passageIcons', ['$parse', function ($parse) {
+	return {
+		compile: function($templateElement, $templateAttributes) {
+			return function ($scope, $element, attrs) {
+				var passage = ($parse(attrs.passageIcons)($scope));
 
+				if (!passage) {
+					return;
+				}
+
+				$element.attr('data-choices', passage.choices.length);
+
+				if (passage.has_ending()) {
+					$element.attr('data-ending', passage.ending_value);
+				}
+			};
+		}
+	};
+}]);
