fbba7c9eeeaeb141ffa0c2bca3ab1e5ee85bc216
Barrett Alexander <barrett@clockwork.net>
2014-05-05 22:04:20 -0500

    Added tooltips to passage exits, animation to updates and conditions

--- js/directives.js
+++ js/directives.js
@@ -34,19 +34,38 @@ angular.module('storyApp.directives')
 					}
 
 					var choices = passage.choices ? passage.choices.length : 0;
+					var verb = 'Links to ';
+					var noun = 'a passage with ';
+					var count = '';
+					if (choices == 0) {
+						count = ' no choices.';
+					}
+					else if (choices == 1) {
+						count = ' one choice.';
+					}
+					else {
+						count = choices + ' choices';
+					}
 
 					if (passage.has_append()) {
+						subject = 'Appends to ';
 						choices = 'a';
 					}
 
-					$element.attr('data-choices', choices);
-
 					if (passage.has_ending()) {
 						$element.attr('data-ending', passage.ending_value);
-
+						noun = 'an ending passage with ';
 					} else {
 						$element.removeAttr('data-ending');
 					}
+
+					var direction = $element.hasClass('passageBack') ? 'left' : 'right';
+
+					$element.attr('data-choices', choices);
+					$element.attr('data-toggle', 'tooltip');
+					$element.attr('data-placement', 'auto ' + direction);
+					$element.attr('title', verb + noun + count);
+					$element.tooltip({container: 'body'});
 				}, true);
 			};
 		}
