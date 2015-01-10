bedff136259ea716be67d36c5a56da9855f794ef
Barrett Alexander <barrett@clockwork.net>
2015-01-10 17:30:28 -0600

    Fixed ng-class typo

--- source/app/stories/story.controller.js
+++ source/app/stories/story.controller.js
@@ -252,12 +252,12 @@
 				return;
 			}
 
-			if (passage.exitIsEmpty()) {
-				passage.setExitType(aExitType);
+			if (aPassage.exitIsEmpty()) {
+				aPassage.setExitType(aExitType);
 				return;
 			}
 
-			switch (passage.exitType) {
+			switch (aPassage.exitType) {
 				case 'ending':
 					alert = 'This passage\'s ending value will be deleted.';
 					break;
@@ -288,7 +288,7 @@
 
 			onConfirm = function () {
 				$scope.$apply(function () {
-					passage.setExitType(aExitType);
+					aPassage.setExitType(aExitType);
 				});
 			};
 
