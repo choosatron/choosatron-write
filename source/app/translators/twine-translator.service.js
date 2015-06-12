2cc3be81c4221f1082cd695529e21138cda0182e
jerrytron <jerry@monkeytheater.com>
2015-06-12 11:49:34 -0500

    This should fix all major issues with Twine story import (fixes #1). Keep in mind, this is meant for simple stories. Choices always come after body text, and one choice per line. You can use simple links [[Link Name]] or complex [[Link Name|link-name]], and use [[<append>|link-name]] as a special append keyword, or <continue> to replace it with "Continue..." automatically.

--- source/app/translators/twine-translator.service.js
+++ source/app/translators/twine-translator.service.js
@@ -8,15 +8,15 @@ function(Story, Passage, Choice) {
 		importMenuTitle: 'Import from Twine',
 		imports: [ 'txt', 'twee', 'tw' ],
 		import: function(data) {
-			var lines = data.split('\n');
-			var line = '';
+			var tweePassages = data.split(/(?=:: )/);
+			var tweeLines = [];
 
 			// reg ex identifiers
-			// Match two items from the ID, the name of the passage, and all tags if any.
-			var reId = /::\s+(\S+)(?:\s+\[([\S]+\:[\S]+(?:[\s][\S]+\:[\S]+)*)\])?/;
+			// Match two items from the ID, the name of the passage, and the clump of tags.
+			var reId = /::\s+([^\[\s]+(?:\s+[^\[\s]+)*)(?:\s*\[([\S]+\:[\S]+(?:[\s][\S]+\:[\S]+)*)\])?/;
 			//var reId = /:: (.+)/;
 			//var reAttributes = /\s(\[([^\]\[]+)\])\s/;
-			var reChoice = /\[\[(.+)\]\]/;
+			var reChoice = /^\[\[(.+)\]\]/;
 
 			console.log('Twine import:');
 
@@ -30,48 +30,79 @@ function(Story, Passage, Choice) {
 				}
 				choice.content = str.substr(0, pipe);
 				choice.destination = str.substr(pipe + 1);
+				console.log("Choice content: " + choice.content);
+				console.log("Choice dest: " + choice.destination);
 				return choice;
 			}
 
 			// Looks through the content of the passage for choices
-			function parseChoices(passage) {
-				if (!passage.content) {
+			function parseChoices(aPassage, aTweeLines) {
+				if (aTweeLines.length === 0) {
 					return;
 				}
-				var contents = passage.content.split('\n');
+				//var contents = passage.content.split('\n');
 				var content = null;
-				passage.content = '';
-				while (contents.length) {
-					content = contents.shift();
-					var match = reChoice.exec(content);
-					if (match) {
-						var choice = makeChoice(match[1]);
-						passage.addChoice(choice);
-					}
-					else {
-						passage.content += content;
+
+				aPassage.content = '';
+
+				var hasChoices = false;
+				for (var i = 0; i < aTweeLines.length; i++) {
+					if (reChoice.test(aTweeLines[i].trim())) {
+						hasChoices = true;
+						aPassage.content = aTweeLines.slice(0, i - 1).join('\n');
+						aTweeLines = aTweeLines.slice(i);
+						break;
 					}
 				}
 
-				if (passage.choices.length === 0) {
-					passage.exitType = 'ending';
-					// Translate the ending quality.
-					if (typeof passage.tags.eq != 'undefined') {
-						passage.setEnding(passage.tags.eq - 3);
+				if (hasChoices === false) {
+					aPassage.content = aTweeLines.join('\n');
+					aTweeLines = [];
+				}
+
+				// Cycle through content lines until we hit a choice (which must be on it's own line).
+				/*while (aTweeLines.length) {
+					if (reChoice.test(aTweeLines[0])) {
+						break;
 					} else {
-						passage.setEnding(0);
+						aPassage.content += aTweeLines.shift();
 					}
-					console.log("Ending Val: " + passage.endingValue);
-				} else if (passage.choices.length === 1) {
-					console.log(passage.choices[0].content);
-					console.log(passage.choices[0]);
-					if (passage.choices[0].content == '<append>') {
-						passage.exitType = 'append';
-						passage.appendLink = passage.choices[0];
+				}*/
+				console.log("Content: " + aPassage.content);
+
+				// Parse remaining choice lines.
+				while (aTweeLines.length) {
+					content = aTweeLines.shift().trim();
+					var match = reChoice.exec(content);
+					console.log(content);
+					if (match) {
+						console.log("Is choice: " + match[1]);
+						var choice = makeChoice(match[1]);
+						aPassage.addChoice(choice);
+					} else {
+						console.log("Ignoring non-choice line: " + content);
+					}
+				}
+
+				if (aPassage.choices.length === 0) {
+					aPassage.exitType = 'ending';
+					// Translate the ending quality.
+					if (typeof aPassage.tags.eq != 'undefined') {
+						aPassage.setEnding(passage.tags.eq - 3);
+					} else {
+						aPassage.setEnding(0);
+					}
+					console.log("Ending Val: " + aPassage.endingValue);
+				} else if (aPassage.choices.length === 1) {
+					console.log(aPassage.choices[0].content);
+					console.log(aPassage.choices[0]);
+					if (aPassage.choices[0].content == '<append>') {
+						aPassage.exitType = 'append';
+						aPassage.appendLink = aPassage.choices[0];
 						console.log("Appended");
-					} else if (passage.choices[0].content == '<continue>') {
-						passage.exitType = 'choices';
-						passage.choices[0].setContent = 'Continue...';
+					} else if (aPassage.choices[0].content == '<continue>') {
+						aPassage.exitType = 'choices';
+						aPassage.choices[0].content = 'Continue...';
 						console.log("Continue");
 					}
 				}
@@ -79,61 +110,70 @@ function(Story, Passage, Choice) {
 
 			var story = new Story();
 			var passage = null;
-			while (lines.length) {
-				line = lines.shift().trim();
-				//if (!line.length) continue;
-				while (reId.test(line) === false) {
+			while (tweePassages.length) {
+				tweeLines = tweePassages.shift().trim().split('\n');
+
+				var tweeTitle = tweeLines.shift().trim();
+				/*while (reId.test(line) === false) {
 					line = lines.shift().trim();
 					console.log(line);
-				}
+				}*/
 
-				var id = reId.exec(line);
-				console.log("Id: " + id[1]);
+				var id = reId.exec(tweeTitle);
 				if (!id) continue;
 				switch (id[1].trim()) {
 					case('StoryAuthor'):
-						story.author = lines.shift();
-						console.log("Author: " + story.author);
+						story.author = tweeLines.join('');
+						//console.log("Author: " + story.author);
 						break;
 					case('StoryTitle'):
-						story.title = lines.shift();
-						console.log("Title: " + story.title);
+						story.title = tweeLines.join('');
+						//console.log("Title: " + story.title);
 						break;
 					case('StorySubtitle'):
-						story.subtitle = lines.shift();
-						console.log("Subtitle: " + story.subtitle);
+						story.subtitle = tweeLines.join('');
+						//console.log("Subtitle: " + story.subtitle);
 						break;
 					default: // Parse the text!
 						passage = new Passage();
 						var content = '';
 						//var attrs = reAttributes.exec(id);
-						passage.id = id[1];
+						passage.id = id[1].trim();
+
+						if (passage.id === 'Start') {
+							passage.opening = true;
+						}
 
 						if (typeof id[2] === 'undefined') {
 							console.log("No tags for passage: " + passage.id);
 						} else {
-							console.log(id[2]);
+							//console.log(id[2]);
 							var tagItems = [];
 							var cur, pair;
 							tagItems = id[2].split(' ');
+							console.log(tagItems);
 							for (var i = 0; i < tagItems.length; i++) {
 								pair = tagItems[i].split(':');
-								passage.tags[pair[0]] = pair[1];
+								console.log("Items: " + pair);
+								if (pair.length === 2) {
+									passage.tags[pair[0]] = pair[1];
+								}
 								//console.log("Tag: " + pair[0] + ", Val: " + pair[1]);
 							}
 						}
 
 						//if (attrs) passage.tags = attrs[1];
 
-						while (lines.length) {
+						/*while (lines.length) {
 							content = lines.shift().trim();
 							if (reId.test(content)) {
 								lines.unshift(content);
 								break;
 							}
 							passage.content += content + '\n';
-						}
-						parseChoices(passage);
+						}*/
+						parseChoices(passage, tweeLines);
+						// Add the passage.
 						story.addPassage(passage);
 				}
 			}
