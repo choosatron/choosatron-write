8f5ea14563c827a6f7c477435e2d29107e7affec
jerrytron <jerry@monkeytheater.com>
2015-06-12 17:10:15 -0500

    Adding objects for managing global strings / utility functions / etc.

---
+++ source/app/shared/cdam-utilities.js
@@ -0,0 +1,16 @@
+(function () {
+	"use strict";
+
+	window.CDAM = window.CDAM || {};
+
+	CDAM.Math = {
+		helperConst: 5,
+		helperFunc: function() {
+			// do stuff.
+		}
+	};
+}());
+
+
+
+//CDAM.Math.helpfunc();
